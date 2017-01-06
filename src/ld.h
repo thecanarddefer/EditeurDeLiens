74a1fca865b86b90415c489e32e5167a3a8779a4
RobinExtrant <robin.extrant@gmail.com>
2017-01-06 15:14:46 +0100

    etape 6: fusion des sections progbits + ecriture du fichier

--- src/ld.h
+++ src/ld.h
@@ -1,7 +1,23 @@
 #ifndef _LD_H_
 #define _LD_H_
 
+#include <elf.h>
+
+
+typedef struct
+{
+	Elf32_Shdr *ptr_shdr1;
+	Elf32_Shdr *ptr_shdr2;
+	Elf32_Word size;
+	Elf32_Off offset;
+} Fusion;
+
+/**
+ * Écrit une section PROGBITS
+ *
+ * @param ehdr: une structure de type Elf32_Ehdr initialisée
+ **/
+int write_progbits_in_file(int fd_in, int fd_out, Elf32_Word size, Elf32_Off offset);
 
 
 #endif
-
